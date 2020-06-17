8f0520daee298b23e96b08085799c8cfd60f0b08
manson125 <manson125@outlook.kr>
2020-06-17 16:55:33 +0900

    add SharedPreferance

--- app/src/main/java/com/j2d2/insulin/InsulinActivity.kt
+++ app/src/main/java/com/j2d2/insulin/InsulinActivity.kt
@@ -2,33 +2,68 @@ package com.j2d2.insulin
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 import com.j2d2.R
 import kotlinx.android.synthetic.main.activity_insulin.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
 
 class InsulinActivity : AppCompatActivity() {
     var appDatabase: AppDatabase? = null
+    val PREPERANCE_FILE: String = "EncrypedInfo"
+
+    companion object {
+        fun isOverMashmellow(): Boolean {
+            if(Build.VERSION <= 23) {
+                return false
+            }
+            return true
+        }
+    }
+
+    private val sharedEnPreferences: SharedPreferences by lazy {
+        val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+        val masterKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+
+        EncryptedSharedPreferences.create(
+            PREPERANCE_FILE,
+            masterKeyAlias,
+            applicationContext,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM)
+    }
+
+    private val sharedPreferences: SharedPreferences by lazy {
+        this@InsulinActivity?.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_insulin)
 
         appDatabase = AppDatabase.getInstance(this)
-        this.setDataEventListener()
-        this.setDateTimeListener()
-        this.setCurrentDate()
-        this.setCurrentTime()
+        setDataEventListener()
+        setDateTimeListener()
+        setCurrentDate()
+        setCurrentTime()
     }
 
+    /**
+     * 현재 날짜값 설정
+     */
     private fun setCurrentDate() {
         val cal = Calendar.getInstance()
         val myFormat = "yyyy-MM-dd"
@@ -36,6 +71,9 @@ class InsulinActivity : AppCompatActivity() {
         editTextDate.setText(sdf.format(cal.time))
     }
 
+    /**
+     * 현재 시간값 설정
+     */
     private fun setCurrentTime() {
         val cal = Calendar.getInstance()
         val myFormat = "HH:mm" // mention the format you needa
@@ -43,15 +81,79 @@ class InsulinActivity : AppCompatActivity() {
         editTextTime.setText(sdf.format(cal.time))
     }
 
+    /**
+     * 인슐린 주사 날짜
+     * @since 2020.06.17
+     * @author perry912
+     * @return date
+     */
+    private fun getCurrentDate(): String {
+        return editTextDate.text.toString().replace("-","")
+    }
+
+    /**
+     * 인슐린 주사 시간
+     * @since 2020.06.17
+     * @author perry912
+     * @return time
+     */
+    private fun getCurrentTime(): String {
+        return editTextTime.text.toString().replace(":","")
+    }
+
+    /**
+     * 인슐린 타입
+     * @since 2020.06.17
+     * @author perry912
+     * @return 0:휴물린엔, 1:캐닌슐린
+     */
+    private fun getInsulinType(): Int {
+        return if(rdoHumulinN.isChecked) 0 else 1
+    }
+
+    /**
+     * 인슐린 원액량
+     * @since 2020.06.17
+     * @author perry912
+     * @return float type
+     */
+    private fun getUndilutedCapacity(): Float {
+        return editUndiluted.text.toString().toFloat()
+    }
+
+    /**
+     * 주사량
+     * @since 2020.06.17
+     * @author perry912
+     * @return float type
+     */
+    private fun getTotalInjectionCapacity(): Float {
+        return editTotalCapacity.text.toString().toFloat()
+    }
+
+    /**
+     * 희석여부
+     * @since 2020.06.17
+     * @author perry912
+     * @return 1:희석, 0:희석 안함
+     */
+    private fun isDiluted(): Int {
+        return if(chkDilution.isChecked) 1 else 0
+    }
+
+    private fun getMemo(): String {
+        return editRemark.text.toString()
+    }
+
     private fun setDataEventListener() {
         btnInsert.setOnClickListener {
             if(editUndiluted.text.trim().isEmpty()) {
-                Toast.makeText(this@InsulinActivity, "원액 용량을 입력하세요.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@InsulinActivity, getString(R.string.com_j2de_insulin_ins_message_undiluted_capacity), Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             if(editTotalCapacity.text.trim().isEmpty()) {
-                Toast.makeText(this@InsulinActivity, "인슐린 주사량을 입력하세요.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@InsulinActivity, getString(R.string.com_j2de_insulin_ins_message_total_insulin_capacity), Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
@@ -59,15 +161,20 @@ class InsulinActivity : AppCompatActivity() {
                 appDatabase?.insulinDao()?.insert(
                     Insulin(
                         uid = 0,
-                        date = editTextDate.text.toString().replace("-",""),
-                        time = editTextTime.text.toString().replace(":",""),
-                        type = if(rdoDried.isChecked) 0 else 1,
-                        undiluted = editUndiluted.text.toString().toFloat(),
-                        totalCapacity = editTotalCapacity.text.toString().toFloat(),
-                        dilution = if(chkDilution.isChecked) 1 else 0,
-                        remark = editRemark.text.toString()
+                        date = getCurrentDate(),
+                        time = getCurrentTime(),
+                        type = getInsulinType(),
+                        undiluted = getUndilutedCapacity(),
+                        totalCapacity = getTotalInjectionCapacity(),
+                        dilution = isDiluted(),
+                        remark = getMemo()
                     )
                 )
+
+                GlobalScope.launch(Dispatchers.Main) {
+                    setLatestInputDataIntoPreference()
+                    Toast.makeText(this@InsulinActivity, getString(R.string.com_j2de_insulin_ins_message_input_complete), Toast.LENGTH_LONG).show()
+                }
             }
         }
 
@@ -100,11 +207,52 @@ class InsulinActivity : AppCompatActivity() {
         }
     }
 
+    /**
+     * @author perry912
+     * @since 2020.06.17
+     * 현재 입력 값 암호화해서 Preference에 저장
+     */
+    private fun setLatestInputDataIntoPreference() {
+
+        try {
+            val sharedPrefsEditor = sharedEnPreferences.edit()
+
+            sharedPrefsEditor.putInt(R.string.com_j2d2_insulin_ins_type.toString(), getInsulinType())
+            sharedPrefsEditor.putFloat(R.string.com_j2d2_insulin_ins_undiluted_capacity.toString(), getUndilutedCapacity())
+            sharedPrefsEditor.putFloat(R.string.com_j2de_insulin_ins_message_total_insulin_capacity.toString(), getTotalInjectionCapacity())
+            sharedPrefsEditor.putInt(R.string.com_j2d2_insulin_ins_dilution.toString(), isDiluted())
+            sharedPrefsEditor.putString(R.string.com_j2d2_insulin_ins_memo.toString(), getMemo())
+        }
+    }
+
+    private fun getLatestInputDataFromPreference() {
+        val sharedPrefsReader = sharedEnPreferences
+
+        if(sharedPrefsReader.getInt(R.string.com_j2d2_insulin_ins_type.toString(), 0) == 0) {
+            rdoHumulinN.callOnClick()
+        } else {
+            rdoCaninsulin.callOnClick()
+        }
+
+        editUndiluted.setText(sharedPrefsReader.getFloat(R.string.com_j2d2_insulin_ins_undiluted_capacity.toString(), 0.0F).toString())
+        editTotalCapacity.setText(sharedPrefsReader.getFloat(R.string.com_j2de_insulin_ins_message_total_insulin_capacity.toString(),0.0F).toString())
+
+        if(sharedPrefsReader.getInt(R.string.com_j2d2_insulin_ins_dilution.toString(), 0) == 0) {
+            rdoHumulinN.callOnClick()
+        }
+
+        editRemark.setText(sharedPrefsReader.getString(R.string.com_j2d2_insulin_ins_memo.toString(), ""))
+//        type = if(rdoHumulinN.isChecked) 0 else 1,
+//        undiluted = editUndiluted.text.toString().toFloat(),
+//        totalCapacity = editTotalCapacity.text.toString().toFloat(),
+//        dilution = if(chkDilution.isChecked) 1 else 0,
+//        remark = editRemark.text.toString()
+    }
+
     private fun setDateTimeListener() {
         editTextDate.setOnTouchListener { _: View, event:MotionEvent ->
             when(event.action) {
                 MotionEvent.ACTION_DOWN -> {
-//                    Toast.makeText(this@MainActivity, "focused", Toast.LENGTH_SHORT).show()
                     val cal = Calendar.getInstance()
                     val y = cal.get(Calendar.YEAR)
                     val m = cal.get(Calendar.MONTH)
@@ -127,7 +275,6 @@ class InsulinActivity : AppCompatActivity() {
         editTextTime.setOnTouchListener { _: View, event:MotionEvent ->
             when(event.action) {
                 MotionEvent.ACTION_DOWN -> {
-//                    Toast.makeText(this@MainActivity, "focused", Toast.LENGTH_SHORT).show()
                     val c:Calendar= Calendar.getInstance()
                     val hh=c.get(Calendar.HOUR_OF_DAY)
                     val mm=c.get(Calendar.MINUTE)
