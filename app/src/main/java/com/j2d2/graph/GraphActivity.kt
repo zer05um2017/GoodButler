69ec1763a9dc01868d8f065e2e59ace16ffa0e24
manson125 <manson125@outlook.kr>
2020-06-25 16:58:35 +0900

    changed graph

--- app/src/main/java/com/j2d2/graph/GraphActivity.kt
+++ app/src/main/java/com/j2d2/graph/GraphActivity.kt
@@ -13,14 +13,13 @@ import com.github.mikephil.charting.components.LimitLine
 import com.github.mikephil.charting.components.LimitLine.LimitLabelPosition
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.components.YAxis
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.IFillFormatter
 import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.github.mikephil.charting.utils.Utils
 import com.j2d2.R
 import com.j2d2.bloodglucose.BloodGlucose
@@ -46,7 +45,9 @@ class GraphActivity : AppCompatActivity(), InvalidateData, OnChartValueSelectedL
         supportActionBar?.hide()
         setButtonEvent()
         loadLatestData()
-        lineChart.setOnChartValueSelectedListener(this)
+        combChart.setOnChartValueSelectedListener(this)
+//        testData1()
+//        testData2()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -118,15 +119,15 @@ class GraphActivity : AppCompatActivity(), InvalidateData, OnChartValueSelectedL
                 i++
             }
 
-            val xAxis: XAxis  = lineChart.xAxis
+            val xAxis: XAxis  = combChart.xAxis
             xAxis.labelCount = glucoseList?.size - 1 ?: 0
             xAxis.valueFormatter = xValsDateLabel?.let { MyValueFormatter(it) }
             // vertical grid lines
             xAxis.enableGridDashedLine(10f, 10f, 0f)
             var yAxis: YAxis
-            yAxis = lineChart.getAxisLeft()
+            yAxis = combChart.getAxisLeft()
             // disable dual axis (only use LEFT axis)
-            lineChart.getAxisRight().setEnabled(false)
+            combChart.getAxisRight().setEnabled(false)
             // horizontal grid lines
             yAxis.enableGridDashedLine(10f, 10f, 0f)
             // axis range
@@ -158,15 +159,151 @@ class GraphActivity : AppCompatActivity(), InvalidateData, OnChartValueSelectedL
             yAxis.addLimitLine(ll1)
             yAxis.addLimitLine(ll2)
             //xAxis.addLimitLine(llXAxis);
-            lineChart.setDrawGridBackground(false)
+            combChart.setDrawGridBackground(false)
 //            lineChart.setGridBackgroundColor(0)
-            lineChart.data = glucoseList?.let { makeLineDataSet(it) }
+            var combData = CombinedData()
+            combData.setData(glucoseList?.let { makeLineDataSet(it) })
+            combData.setData(generateScatterData())
+            combData.setData(generateBarData())
+            combChart.data = combData
             CoroutineScope(Dispatchers.Main).launch {
-                lineChart.animateX(500)
+                combChart.animateX(500)
 //                invalidate(lineChart = lineChart)
             }
         }
     }
+
+//    private fun testData1() {
+//        barChartForInsulin.getDescription().setEnabled(false)
+//
+//        // if more than 60 entries are displayed in the chart, no values will be
+//        // drawn
+//
+//        // if more than 60 entries are displayed in the chart, no values will be
+//        // drawn
+//        barChartForInsulin.setMaxVisibleValueCount(60)
+//
+//        // scaling can now only be done on x- and y-axis separately
+//
+//        // scaling can now only be done on x- and y-axis separately
+//        barChartForInsulin.setPinchZoom(false)
+//
+//        barChartForInsulin.setDrawBarShadow(false)
+//        barChartForInsulin.setDrawGridBackground(false)
+//
+//        val xAxis: XAxis = barChartForInsulin.getXAxis()
+//        xAxis.position = XAxisPosition.BOTTOM
+//        xAxis.setDrawGridLines(false)
+//
+//        barChartForInsulin.getAxisLeft().setDrawGridLines(false)
+//
+//        // setting data
+//
+//        // setting data
+//        val values = arrayListOf<BarEntry>()
+//
+//        for (i in 0 until 2) {
+//            val multi: Float = (2 + 1).toFloat()
+//            val val1 = (Math.random() * multi).toFloat() + multi / 3
+//            val barE = BarEntry(i.toFloat(), val1)
+//            values.add(barE)
+//        }
+//
+//        val set1: BarDataSet
+//
+//        if (barChartForInsulin.getData() != null &&
+//            barChartForInsulin.getData().getDataSetCount() > 0
+//        ) {
+//            set1 =
+//                barChartForInsulin.data.getDataSetByIndex(0) as BarDataSet// .getData().getDataSetByIndex(0)
+//            set1.values = values
+//            barChartForInsulin.getData().notifyDataChanged()
+//            barChartForInsulin.notifyDataSetChanged()
+//        } else {
+//            set1 = BarDataSet(values, "Data Set")
+//            set1.setColors(*ColorTemplate.VORDIPLOM_COLORS)
+//            set1.setDrawValues(false)
+//            val dataSets = java.util.ArrayList<IBarDataSet>()
+//            dataSets.add(set1)
+//            val data = BarData(dataSets)
+//            barChartForInsulin.setData(data)
+//            barChartForInsulin.setFitBars(true)
+//        }
+//
+//        barChartForInsulin.invalidate()
+//        // add a nice and smooth animation
+//
+//        // add a nice and smooth animation
+//        barChartForInsulin.animateY(500)
+//
+//        barChartForInsulin.getLegend().setEnabled(false)
+//    }
+//
+//    private fun testData2() {
+//        barChartForFeeding.getDescription().setEnabled(false)
+//
+//        // if more than 60 entries are displayed in the chart, no values will be
+//        // drawn
+//
+//        // if more than 60 entries are displayed in the chart, no values will be
+//        // drawn
+//        barChartForFeeding.setMaxVisibleValueCount(60)
+//
+//        // scaling can now only be done on x- and y-axis separately
+//
+//        // scaling can now only be done on x- and y-axis separately
+//        barChartForFeeding.setPinchZoom(false)
+//
+//        barChartForFeeding.setDrawBarShadow(false)
+//        barChartForFeeding.setDrawGridBackground(false)
+//
+//        val xAxis: XAxis = barChartForFeeding.getXAxis()
+//        xAxis.position = XAxisPosition.BOTTOM
+//        xAxis.setDrawGridLines(false)
+//
+//        barChartForFeeding.getAxisLeft().setDrawGridLines(false)
+//
+//        // setting data
+//
+//        // setting data
+//        val values = arrayListOf<BarEntry>()
+//
+//        for (i in 0 until 2) {
+//            val multi: Float = (2 + 1).toFloat()
+//            val val1 = (Math.random() * multi).toFloat() + multi / 3
+//            val barE = BarEntry(i.toFloat(), val1)
+//            values.add(barE)
+//        }
+//
+//        val set1: BarDataSet
+//
+//        if (barChartForFeeding.getData() != null &&
+//            barChartForFeeding.getData().getDataSetCount() > 0
+//        ) {
+//            set1 =
+//                barChartForFeeding.data.getDataSetByIndex(0) as BarDataSet// .getData().getDataSetByIndex(0)
+//            set1.values = values
+//            barChartForFeeding.getData().notifyDataChanged()
+//            barChartForFeeding.notifyDataSetChanged()
+//        } else {
+//            set1 = BarDataSet(values, "Data Set")
+//            set1.setColors(*ColorTemplate.VORDIPLOM_COLORS)
+//            set1.setDrawValues(false)
+//            val dataSets = java.util.ArrayList<IBarDataSet>()
+//            dataSets.add(set1)
+//            val data = BarData(dataSets)
+//            barChartForFeeding.setData(data)
+//            barChartForFeeding.setFitBars(true)
+//        }
+//
+//        barChartForFeeding.invalidate()
+//        // add a nice and smooth animation
+//
+//        // add a nice and smooth animation
+//        barChartForFeeding.animateY(500)
+//
+//        barChartForFeeding.getLegend().setEnabled(false)
+//    }
 
     private fun makeLineDataSet(value: ArrayList<Entry>):LineData {
         val set1 = LineDataSet(value, getString(R.string.com_j2d2_graph_graph_title))
@@ -192,7 +329,7 @@ class GraphActivity : AppCompatActivity(), InvalidateData, OnChartValueSelectedL
         // set the filled area
         set1.setDrawFilled(true)
         set1.setFillFormatter(IFillFormatter { dataSet, dataProvider ->
-            lineChart.getAxisLeft().getAxisMinimum()
+            combChart.getAxisLeft().getAxisMinimum()
         })
         // set color of filled area
         if (Utils.getSDKInt() >= 18) {
@@ -206,6 +343,60 @@ class GraphActivity : AppCompatActivity(), InvalidateData, OnChartValueSelectedL
         val dataSets = arrayListOf<ILineDataSet>()
         dataSets.add(set1) // add the data sets
         return LineData(dataSets)
+    }
+
+    private fun generateScatterData():ScatterData {
+
+        var d = ScatterData();
+
+        var entries = arrayListOf<Entry>()
+
+        for (i in 0..8)
+            entries.add(Entry(i + 0.25f, ((Math.random()* 100) + 55).toFloat()));
+
+        var set = ScatterDataSet(entries, "Scatter DataSet");
+        set.setColors(*ColorTemplate.MATERIAL_COLORS)
+        set.setScatterShapeSize(7.5f);
+        set.setDrawValues(false);
+        set.setValueTextSize(10f);
+        d.addDataSet(set);
+
+        return d;
+    }
+
+    private fun generateBarData(): BarData? {
+        val entries1 = java.util.ArrayList<BarEntry>()
+        val entries2 = java.util.ArrayList<BarEntry>()
+        for (index in 0 until 8) {
+            entries1.add(BarEntry(0.0f, ((Math.random()* 100) + 55).toFloat()))
+
+            // stacked
+            entries2.add(BarEntry(0.0f, floatArrayOf(((Math.random()* 100) + 55).toFloat(), ((Math.random()* 100) + 55).toFloat())))
+        }
+        val set1 = BarDataSet(entries1, "Bar 1")
+        set1.color = Color.rgb(60, 220, 78)
+        set1.valueTextColor = Color.rgb(60, 220, 78)
+        set1.valueTextSize = 10f
+        set1.axisDependency = YAxis.AxisDependency.LEFT
+        val set2 = BarDataSet(entries2, "")
+        set2.stackLabels = arrayOf("Stack 1", "Stack 2")
+        set2.setColors(
+            Color.rgb(61, 165, 255),
+            Color.rgb(23, 197, 255)
+        )
+        set2.valueTextColor = Color.rgb(61, 165, 255)
+        set2.valueTextSize = 10f
+        set2.axisDependency = YAxis.AxisDependency.LEFT
+        val groupSpace = 0.06f
+        val barSpace = 0.02f // x2 dataset
+        val barWidth = 0.45f // x2 dataset
+        // (0.45 + 0.02) * 2 + 0.06 = 1.00 -> interval per "group"
+        val d = BarData(set1, set2)
+        d.barWidth = barWidth
+
+        // make this BarData object grouped
+        d.groupBars(0f, groupSpace, barSpace) // start at x = 0
+        return d
     }
 
     private fun setButtonEvent() {
